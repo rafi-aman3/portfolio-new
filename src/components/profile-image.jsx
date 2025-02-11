99ec9edf03199be6ea4e7c24b17532be6cdab7f1
rafi-aman3 <rafiaman03@gmail.com>
2025-02-12 00:58:07 +0600

    fix: projects side design done

--- src/components/profile-image.jsx
+++ src/components/profile-image.jsx
@@ -1,10 +1,24 @@
+import Image from "next/image";
 import React from "react";
 
 export const ProfileImage = () => {
 	return (
 		<div className='w-full h-full rounded-xl md:row-span-4 md:col-span-4  lg:row-span-5 lg:col-span-3  bg-slate-200 sm:bg-blue-200 md:bg-green-200 lg:bg-orange-200 xl:bg-slate-200 overflow-hidden'>
 			<div className='h-full w-full'>
-				<div className='h-full w-full relative aspect-[4/3] group'></div>
+				<div className='h-full overflow-hidden rounded-xl w-full relative aspect-[4/3] group'>
+					<Image
+						className='object-cover rounded-xl group-hover:scale-125  transition-all ease-lienar duration-700'
+						fill
+						sizes='100vw'
+						src={"/images/profile.jpg"}
+						alt='profile-image'
+						quality={75}
+						priority={false}
+					/>
+					<div className='group-hover:opacity-0 group-hover:translate-y-10 absolute text-primary-foreground opacity-50 bottom-0 flex items-center justify-center w-full pb-4 transition-all '>
+						( hover on me )
+					</div>
+				</div>
 			</div>
 		</div>
 	);
--- src/components/project-container.jsx
+++ src/components/project-container.jsx
@@ -1,9 +1,156 @@
-import React from "react";
-
 export const ProjectContainer = () => {
 	return (
 		<div className='w-full h-full rounded-xl md:row-span-5 md:col-span-6  lg:row-span-8 lg:col-span-3 bg-secondary sm:bg-blue-200 md:bg-green-200 lg:bg-orange-200 xl:bg-secondary'>
-			<div className='h-full w-full relative group overflow-hidden rounded-md p-1'></div>
+			<div className='h-full w-full relative group overflow-hidden rounded-md p-1'>
+				<div className='h-full w-full  overflow-auto no-scrollbar'>
+					<div className='flex flex-col h-full'>
+						<div className=' h-full w-full'>
+							<div className='h-full w-full p-1'>
+								<div className='rounded-xl bg-card text-card-foreground shadow h-full overflow-hidden border-background border-2'>
+									<div className='flex relative aspect-video items-center justify-center p-4'>
+										<div className='absolute inset-0 bg-gradient-to-b from-transparent to-background h-full w-full z-20' />
+										<div className='absolute inset-0   z-10'>
+											<img
+												style={{
+													position: "relative",
+													width: "100%",
+													height: "100%",
+													left: 0,
+													top: 0,
+													right: 0,
+													bottom: 0,
+													color: "transparent",
+													objectFit: "cover",
+												}}
+												fill='true'
+												src={`/images/hriscoreweb.webp`}
+												quality={75}
+												sizes='100vw'
+											/>
+										</div>
+
+										<div className='relative  z-30   flex flex-col justify-between select-none '>
+											<div className=' flex justify-end p-2 text-2xl font-semibold text-primary '>
+												<div className='bg-foreground p-1 rounded-full px-4'>
+													Web
+												</div>
+											</div>
+											<div className='p-2'>
+												<div className=' flex justify-start p-2 text-md font-bold text-foreground'>
+													Health Assessment System
+												</div>
+												<div className=' flex justify-start p-2 text-sm text-foreground'>
+													Design and develop web application for employee health
+													assessment to avoid exposure of Covid-19, web
+													application has several features such as AD Auth,
+													Integrate HRIS, Health condition calculation,
+													Dashboard and Reports
+												</div>
+											</div>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+						<div className=' h-full w-full'>
+							<div className='h-full w-full p-1'>
+								<div className='rounded-xl bg-card text-card-foreground shadow h-full overflow-hidden border-background border-2'>
+									<div className='flex relative aspect-video items-center justify-center p-4'>
+										<div className='absolute inset-0 bg-gradient-to-b from-transparent to-background h-full w-full z-20' />
+										<div className='absolute inset-0   z-10'>
+											<img
+												style={{
+													position: "relative",
+													width: "100%",
+													height: "100%",
+													left: 0,
+													top: 0,
+													right: 0,
+													bottom: 0,
+													color: "transparent",
+													objectFit: "cover",
+												}}
+												fill='true'
+												src={`/images/hriscoreweb.webp`}
+												quality={75}
+												sizes='100vw'
+											/>
+										</div>
+
+										<div className='relative  z-30   flex flex-col justify-between select-none '>
+											<div className=' flex justify-end p-2 text-2xl font-semibold text-primary '>
+												<div className='bg-foreground p-1 rounded-full px-4'>
+													Web
+												</div>
+											</div>
+											<div className='p-2'>
+												<div className=' flex justify-start p-2 text-md font-bold text-foreground'>
+													Health Assessment System
+												</div>
+												<div className=' flex justify-start p-2 text-sm text-foreground'>
+													Design and develop web application for employee health
+													assessment to avoid exposure of Covid-19, web
+													application has several features such as AD Auth,
+													Integrate HRIS, Health condition calculation,
+													Dashboard and Reports
+												</div>
+											</div>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+						<div className=' h-full w-full'>
+							<div className='h-full w-full p-1'>
+								<div className='rounded-xl bg-card text-card-foreground shadow h-full overflow-hidden border-background border-2'>
+									<div className='flex relative aspect-video items-center justify-center p-4'>
+										<div className='absolute inset-0 bg-gradient-to-b from-transparent to-background h-full w-full z-20' />
+										<div className='absolute inset-0   z-10'>
+											<img
+												style={{
+													position: "relative",
+													width: "100%",
+													height: "100%",
+													left: 0,
+													top: 0,
+													right: 0,
+													bottom: 0,
+													color: "transparent",
+													objectFit: "cover",
+												}}
+												fill='true'
+												src={`/images/hriscoreweb.webp`}
+												quality={75}
+												sizes='100vw'
+											/>
+										</div>
+
+										<div className='relative  z-30   flex flex-col justify-between select-none '>
+											<div className=' flex justify-end p-2 text-2xl font-semibold text-primary '>
+												<div className='bg-foreground p-1 rounded-full px-4'>
+													Web
+												</div>
+											</div>
+											<div className='p-2'>
+												<div className=' flex justify-start p-2 text-md font-bold text-foreground'>
+													Health Assessment System
+												</div>
+												<div className=' flex justify-start p-2 text-sm text-foreground'>
+													Design and develop web application for employee health
+													assessment to avoid exposure of Covid-19, web
+													application has several features such as AD Auth,
+													Integrate HRIS, Health condition calculation,
+													Dashboard and Reports
+												</div>
+											</div>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
 		</div>
 	);
 };
